d678102f3e9676701b1d1e8cb462c90409e33369
HamzaAryanSapnil <139946462+HamzaAryanSapnil@users.noreply.github.com>
2025-05-03 15:47:15 +0600

    all tool page edited

--- app/(dashboard)/dashboard/tools/all-tools/page.js
+++ app/(dashboard)/dashboard/tools/all-tools/page.js
@@ -3,11 +3,12 @@ import { refreshDataTools } from "@/components/Refresh-Functions/toolsRefreshFun
 import { columns } from "@/components/ReusableTable/column";
 import { DataTable } from "@/components/ReusableTable/data-table";
 import { payments } from "@/data/payments";
-import axios from "axios";
+import { axiosClient } from "@/lib/apiClient";
+
 import React from "react";
 
 const AllToolsPage = async () => {
-  const res = await axios("http://localhost:3000/api/admin/tools");
+  const res = await axiosClient.get("/api/admin/tools");
   const tools = res?.data || [];
 
   return (
